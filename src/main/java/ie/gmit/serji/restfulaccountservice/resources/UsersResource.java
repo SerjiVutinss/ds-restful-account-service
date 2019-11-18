60197fca6b327a5e6983c8ba70d33e2ba6b96c03
SerjiVutinss <SerjiVutinss@gmail.com>
2019-11-18 19:33:21 +0000

    Created getUserById

--- src/main/java/ie/gmit/serji/restfulaccountservice/resources/UsersResource.java
+++ src/main/java/ie/gmit/serji/restfulaccountservice/resources/UsersResource.java
@@ -6,6 +6,7 @@ import ie.gmit.serji.restfulaccountservice.services.UsersDbService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -24,6 +25,18 @@ public class UsersResource {
         List<User> allUsers  = _usersService.getAll();
 
         return Response.ok(allUsers).build();
+    }
+
+    @GET
+    @Path("/{userId}")
+    public Response getUserById(@PathParam("userId") Integer userId){
+
+        User u = _usersService.getOne(userId);
+        if(u!=null){
+            return Response.ok(u).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
     }
 
 }
